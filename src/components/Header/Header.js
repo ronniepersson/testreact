8fd869f598873fe9d2b2b408067bd35ec3b86341
Ronnie Persson <ronnie.persson@silentorder.com>
2016-08-24 11:12:30 +0200

    Testing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import StupidSimple from '../StupidSimple'
 import { IndexLink, Link } from 'react-router'
 import classes from './Header.scss'
 
-export const Header = () => (
+export const Header = (props) => (
   <div>
     <h1>React Redux Starter Kit</h1>
+    <StupidSimple text="hej" {...props} />
     <IndexLink to='/' activeClassName={classes.activeRoute}>
       Home
     </IndexLink>
---
+++ src/components/StupidSimple.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const StupidSimple = ( props ) => (
+    <button onClick={props.click}>{props.text}</button>
+)
+
+export default StupidSimple
